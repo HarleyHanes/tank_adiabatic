Fixed some issues in dvdH, have verified in simplified case that there is error in dydtSens and expected formula is correct.

tankModel/TankModel.py
@@ -480,10 +480,7 @@ class TankModel:
         nPoints=self.nElements*self.nCollocation
         u=y[0:nPoints]
         v=y[nPoints:2*nPoints]
-        dudt=np.dot(self.massRHSmat,u)+self.params["Da"]*(1-u)*np.exp(self.params["gamma"]*self.params["beta"]*v/(1+self.params["beta"]*v))
-        dvdt=np.dot(self.tempRHSmat,v)+(self.params["Da"]*(1-u)*np.exp(self.params["gamma"]*self.params["beta"]*v/(1+self.params["beta"]*v))
-                                    +self.params["delta"]*(self.params["vH"]-v))/self.params["Le"]
-        dydt=np.append(dudt,dvdt)
+        dydt=self.dydt(y[0:2*nPoints],t)
         eqCounter=2
         for param in paramSelect:
             dudParam = y[eqCounter*nPoints:(eqCounter+1)*nPoints]
@@ -492,9 +489,9 @@ class TankModel:
                 case "vH":
                     ddudParamdt=np.dot(self.massRHSmat,dudParam)+self.params["Da"]*np.exp(self.params["gamma"]*self.params["beta"]*v/(1+self.params["beta"]*v))\
                                 *((1-u)*(self.params["gamma"]*self.params["beta"]*(1+2*self.params["beta"]*v*dvdParam/(1+self.params["beta"]*v)**2))-dudParam)
-                    ddvdParamdt=np.dot(self.tempRHSmat,dvdParam)+self.params["Da"]*np.exp(self.params["gamma"]*self.params["beta"]*v/(1+self.params["beta"]*v))\
+                    ddvdParamdt=(np.dot(self.tempRHSmat,dvdParam)+self.params["Da"]*np.exp(self.params["gamma"]*self.params["beta"]*v/(1+self.params["beta"]*v))\
                                 *((1-u)*(self.params["gamma"]*self.params["beta"]*(1+2*self.params["beta"]*v*dvdParam/(1+self.params["beta"]*v)**2))-dudParam)\
-                                +self.params["delta"]-self.params["vH"]*dvdParam
+                                +self.params["delta"]*(1-dvdParam))/self.params["Le"]
                     dydt=np.append(dydt,ddudParamdt)
                     dydt=np.append(dydt,ddvdParamdt)
 

tankModel/test/testSensitivityEquations.py
@@ -15,58 +15,69 @@ import scipy
 print("Testing Sensitivity Equations")
 print("     Testing vH")
 #========================================== vH Tests ==================================================================
-
 #Case 1: Transport and Diffusion Portion
 #Set Da=0 so the nonlinear source term is 0
-params={"PeM": 1, "PeT": 1, "f": 1, "Le": 1, "Da": 0, "beta": 1, "gamma": 1,"delta": 1, "vH":1}
+params={"PeM": 300, "PeT": 100, "f": 0, "Le": 3, "Da": 0, "beta": 0, "gamma": 0,"delta": 2, "vH":1}
 #Define nElem=1,nColl=2 model with roots at -1 and 1 for simplicity
-model = TankModel(nCollocation=2,nElements=1,spacing="legendre",bounds=[-np.sqrt(3),np.sqrt(3)],params=params)
+model = TankModel(nCollocation=2,nElements=2,spacing="legendre",bounds=[0,1],params=params)
 x=model.collocationPoints
 #Case 1a: u=x^2, uvH=x^2, v=x^2, vvH=x^2
-u = lambda x:-x**2+x+1/params["PeM"]
-v = lambda x:-x**2+x+1/(1-params["f"])*(1/params["PeT"]+params["f"]())
-y=np.concatenate((x**2,x**2,x**2,x**2),axis=0)
-print(y)
+u = -x**2+2*x+2/params["PeM"]
+v = -x**2+2*x+(2/params["PeT"]+params["f"]*(2-1))/(1-params["f"])
+dudvH = -x**2+2*x+2/params["PeM"]
+dvdvH = -x**2+2*x+(2/params["PeT"]+params["f"]*(2-1))/(1-params["f"])
+dudvHx = -2*x+2
+dvdvHx = -2*x+2
+dudvHxx = -2
+dvdvHxx = -2
+y=np.concatenate((u,v,dudvH,dvdvH),axis=0)
 dydt = model.dydtSens(y,0,paramSelect=["vH"])
+dudvHtComputed = dydt[2*model.nCollocation*model.nElements:3*model.nCollocation*model.nElements]
+dvdvHtComputed = dydt[3*model.nCollocation*model.nElements:]
 #Check ddudvHdt
-print(dydt)
-print(-2*x+2/params["PeM"])
-assert(np.isclose(dydt[4:6],-2*x+2/params["PeM"]).all())
+assert(np.isclose(dudvHtComputed, -dudvHx+dudvHxx/params["PeM"]                                        ).all())
 #Check ddvdvHdt
-assert(np.isclose(dydt[6:],1/params["Le"]*(-2*x+2/params["PeT"])+params["delta"]-params["vH"]*(x**2)).all())
-
-params={"PeM": 7, "PeT": 31, "f": 1, "Le": 1, "Da": 0, "beta": 1, "gamma": 1,"delta": 8, "vH":9}
-#Define nElem=1,nColl=2 model with roots at -1 and 1 for simplicity
-model = TankModel(nCollocation=2,nElements=1,spacing="legendre",bounds=[-np.sqrt(3),np.sqrt(3)],params=params)
-dydt = model.dydtSens(y,0,paramsSelect=["vH"])
-#Check ddudvHdt
-assert(np.isclose(dydt[4:6],-2*x+2/params["PeM"]).all())
-#Check ddvdvHdt
-assert(np.isclose(dydt[6:],1/params["Le"]*(-2*x+2/params["PeT"])+params["delta"]-params["vH"]*(x**2)).all())
-print("         vH Transport and Diffusion Portion Passes")
+assert(np.isclose(dvdvHtComputed,(-dvdvHx+dvdvHxx/params["PeT"]+params["delta"]*(1-dvdvH))/params["Le"]).all())
 
 #Case 2: Nonlinear Source Portion
-#Can't set transport terms to 0 due to BC but will use linear terms so diffusion term is 0
-params={"PeM": 1, "PeT": 1, "f": 1, "Le": 1, "Da": 1, "beta": 1, "gamma": 1,"delta": 1, "vH":1}
-#Define nElem=1,nColl=2 model 
-model = TankModel(nCollocation=2,nElements=1,spacing="legendre",bounds=[0,1],params=params)
+params={"PeM": 1, "PeT": 1, "f": 0, "Le": 1, "Da": 1, "beta": 1, "gamma": 1,"delta": 0, "vH": 0}
+#Define nElem=1,nColl=2 model with roots at -1 and 1 for simplicity
+model = TankModel(nCollocation=1,nElements=2,spacing="legendre",bounds=[0,1],params=params)
 x=model.collocationPoints
-#Case 2a: u=1, uvH=x, v=x, vvH=x
-y=np.append(1+x*0,x,x,x)
+#Case 1a: u=x^2, uvH=x^2, v=x^2, vvH=x^2
+#u = -x**2+2*x+2/params["PeM"]
+u=0*x
+v = -x**2+2*x+(2/params["PeT"]+params["f"]*(2-1))/(1-params["f"])
+#dudvH = -x**2+2*x+2/params["PeM"]
+dudvH=0*x
+#dvdvH = -x**2+2*x+(2/params["PeT"]+params["f"]*(2-1))/(1-params["f"])
+dudvHx=0*x
+dudvHxx=0*x
+#dudvHx = -2*x+2
+#dvdvHx = -2*x+2
+#dudvHxx = -2
+#dvdvHxx = -2
+dvdvH=0*x
+dvdvHx=0*x
+dvdvHxx=0*x
+y=np.concatenate((u,v,dudvH,dvdvH),axis=0)
+print(y)
 dydt = model.dydtSens(y,0,paramSelect=["vH"])
+dudvHtComputed = dydt[2*model.nCollocation*model.nElements:3*model.nCollocation*model.nElements]
+dvdvHtComputed = dydt[3*model.nCollocation*model.nElements:]
+print(dvdvHtComputed)
+print(1/((-x**2+2*x+3)**2)*np.exp((-x**2+2*x+2)/(-x**2+2*x+3)))
+print((-dvdvHx+dvdvHxx/params["PeT"]\
+                                  +params["Da"]*np.exp(params["gamma"]*params["beta"]*v/(1+params["beta"]*v))\
+                                    *((1-u)*(params["gamma"]*params["beta"]*(1+2*params["beta"]*v*dvdvH)/(1+params["beta"]*v)**2)-dudvH)\
+                                    +params["delta"]*(1-dvdvH))/params["Le"])
 #Check ddudvHdt
-assert(np.isclose(dydt[4:6],-1-params["Da"]*np.exp(params["gamma"]*params["beta"]*x/(1+params["beta"]*x))*x).all())
+# assert(np.isclose(dudvHtComputed, -dudvHx+dudvHxx/params["PeM"]\
+#                                     +params["Da"]*np.exp(params["gamma"]*params["beta"]*v/(1+params["beta"]*v))\
+#                                     *((1-u)*(params["gamma"]*params["beta"]*(1+2*params["beta"]*v*dvdvH)/(1+params["beta"]*v)**2)-dudvH)).all())
 #Check ddvdvHdt
-assert(np.isclose(dydt[6:],1/params["Le"]*(-1-params["Da"]*np.exp(params["gamma"]*params["beta"]*x/(1+params["beta"]*x))*x \
-                                           + params["delta"]-params["vH"]*x)).all())
-
-#Case2b: u =x, uvH=0, v=x^2, vvH=x
-y=np.append(x,x*0,x**2,x)
-dydt = model.dydtSens(y,0,paramSelect=["vH"])
-#Check ddudvHdt
-assert(np.isclose(dydt[4:6],-1-params["Da"]*np.exp(params["gamma"]*params["beta"]*x**2/(1+params["beta"]*x**2))\
-                                *(1-x)*(params["gamma"]*params["beta"]*(1+2*params["beta"]*x**3)/(1+params["beta"]*x**2)**2)).all())
-#Check ddvdvHdt
-assert(np.isclose(dydt[6:],1/params["Le"]*(-1-params["Da"]*np.exp(params["gamma"]*params["beta"]*x**2/(1+params["beta"]*x**2))\
-                                *(1-x)*(params["gamma"]*params["beta"]*(1+2*params["beta"]*x**3)/(1+params["beta"]*x**2)**2)\
-                                +params["delta"]-params["vH"]*x)).all())
+assert(np.isclose(dvdvHtComputed,(-dvdvHx+dvdvHxx/params["PeT"]\
+                                  +params["Da"]*np.exp(params["gamma"]*params["beta"]*v/(1+params["beta"]*v))\
+                                    *((1-u)*(params["gamma"]*params["beta"]*(1+2*params["beta"]*v*dvdvH)/(1+params["beta"]*v)**2)-dudvH)\
+                                    +params["delta"]*(1-dvdvH))/params["Le"]).all())
+print("         vH Transport and Diffusion Portion Passes")